f423e5d49e8696d7f06fa9c9ea4402d6abe41088
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-07-09 12:03:06 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4*a*c;
+
+  if (d === 0) {
+    arr.push(-b/(2*a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d))/(2*a));
+    arr.push((-b - Math.sqrt(d))/(2*a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+  let monthlyPayment = S * (P + (P / (((1 + P)**countMonths) - 1)));
   
+  return +(monthlyPayment * countMonths).toFixed(2);
 }
+
