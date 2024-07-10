3724d48ea5c507bbd3a6a2e373884e190711a0dc
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-07-10 16:39:33 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,51 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  return ( arr.length === 0 ? 0 : { 
+     min: Math.min(...arr),
+     max: Math.max(...arr),
+     avg: +(arr
+            .reduce((sum, current) => sum + current, 0)/arr.length)
+            .toFixed(2)
+          } )
 }
 
 function summElementsWorker(...arr) {
-
+  return (
+    arr.length === 0 ? 0 : arr.reduce((sum, current) => sum + current, 0)
+  )
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  return (
+    arr.length === 0 ? 0 : Math.max(...arr) - Math.min(...arr)
+  )
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  return (  
+    arr.length === 0 ? 0 : arr.reduce((res, current) => current%2 === 0 ? res + current : res - current, 0)
+  )
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length === 0) {
+     return 0 } 
 
+  const evenElements = arr.filter((current) => current%2 === 0);
+  const sumElements = evenElements.reduce((res, current) => res += current, 0);
+
+  return (
+    sumElements/evenElements.length
+  )
 }
 
 function makeWork (arrOfArr, func) {
-
+  let maxWorkerResult = -Infinity;
+  let funcResult = 0;
+  for (let elem of arrOfArr) {
+    funcResult = func(...elem);
+    if (funcResult > maxWorkerResult) { 
+      maxWorkerResult = funcResult;
+    }
+  }
+  return maxWorkerResult;
 }
