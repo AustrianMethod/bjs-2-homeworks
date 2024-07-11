0834df1551e5a785cf2c6a8ce09edca6dcb9bcac
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-07-11 11:47:37 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length !== arr2.length ? false : arr1.every((elem, index) => elem === arr2[index])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return users
+	    .filter((elem) => elem.gender === gender)
+		.reduce((res, current, ind, arr) => res += current.age / arr.length, 0)
 }
