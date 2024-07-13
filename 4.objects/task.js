7b69ff823530372bdc5f1420d1d557285801f41b
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-07-13 13:58:43 +0300

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,27 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    "marks" in this ? this.marks.splice(this.marks.length, 0, ...marks) : undefined;
 }
 
 Student.prototype.getAverage = function () {
-  
+    return (
+        ("marks" in this && this.marks.length !==0) ?
+        (this.marks.reduce((res, elem) => res + elem, 0)/this.marks.length) : 0
+    )
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
